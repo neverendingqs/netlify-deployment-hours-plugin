0d805537c3c3060e21a0f38312f206e4337749f6
Mark Tse <mark.tse@neverendingqs.com>
2019-11-16 19:54:06 -0500

    plugin - pojo => function.

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const CronAllowedRange = require('cron-allowed-range');
 
-module.exports = {
-  name: 'deployment-hours',
-  init: () => {
-    const expression = process.env.DEPLOYMENT_HOURS_EXPRESSION || '* * * * *';
-    const timezone = process.env.DEPLOYMENT_HOURS_TIMEZONE || 'America/Toronto';
+module.exports = function(config) {
+  return {
+    name: 'deployment-hours',
+    init: () => {
+      const expression = process.env.DEPLOYMENT_HOURS_EXPRESSION || '* * * * *';
+      const timezone = process.env.DEPLOYMENT_HOURS_TIMEZONE || 'America/Toronto';
 
-    const cr = new CronAllowedRange(expression, timezone);
-    const now = new Date();
+      const cr = new CronAllowedRange(expression, timezone);
+      const now = new Date();
 
-    console.log(`Current time: '${now}'. Expression: '${expression}'. Timezone: '${timezone}'.`);
+      console.log(`Current time: '${now}'. Expression: '${expression}'. Timezone: '${timezone}'.`);
 
-    if(!cr.isDateAllowed(now)) {
-      throw new Error('Deployment not allowed at this time.');
+      if(!cr.isDateAllowed(now)) {
+        throw new Error('Deployment not allowed at this time.');
+      }
     }
   }
 };
