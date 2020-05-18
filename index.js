f1cc6a3981d096a4e85d108a3a71ce56993c0e04
ehmicky <ehmicky@users.noreply.github.com>
2020-05-18 23:05:07 +0200

    Rename `onInit` (#13)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const CronAllowedRange = require('cron-allowed-range');
 
 module.exports = {
     name: 'deployment-hours',
-    onInit: ({ utils }) => {
+    onPreBuild: ({ utils }) => {
       const expression = process.env.DEPLOYMENT_HOURS_EXPRESSION || '* * * * *';
       const timezone = process.env.DEPLOYMENT_HOURS_TIMEZONE || 'America/Toronto';
 
