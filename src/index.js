5841fd5d4cce5cb2e95334edc493ea36a2f8a684
Mark Tse <mark.tse@neverendingqs.com>
2021-01-31 16:36:54 -0500

    test: eslint init.

--- src/index.js
+++ src/index.js
@@ -2,35 +2,35 @@ const createCronAllowedRange = require('./lib/cron-allowed-range');
 const getConfigValue = require('./lib/get-config-value');
 
 module.exports = {
-    onPreBuild: ({ utils, inputs }) => {
-      const expression = getConfigValue({
-        configName: 'expression',
-        defaultValue: '* * * * *',
-        env: process.env.DEPLOYMENT_HOURS_EXPRESSION,
-        input: inputs.expression
-      });
+  onPreBuild: ({ utils, inputs }) => {
+    const expression = getConfigValue({
+      configName: 'expression',
+      defaultValue: '* * * * *',
+      env: process.env.DEPLOYMENT_HOURS_EXPRESSION,
+      input: inputs.expression
+    });
 
-      const timezone = getConfigValue({
-        configName: 'timezone',
-        defaultValue: 'America/Toronto',
-        env: process.env.DEPLOYMENT_HOURS_TIMEZONE,
-        input: inputs.timezone
-      });
+    const timezone = getConfigValue({
+      configName: 'timezone',
+      defaultValue: 'America/Toronto',
+      env: process.env.DEPLOYMENT_HOURS_TIMEZONE,
+      input: inputs.timezone
+    });
 
-      let cr;
-      try {
-        cr = createCronAllowedRange(expression, timezone);
-      } catch (error) {
-        utils.build.failPlugin('Invalid Cron expression or timezone', { error });
-        return;
-      }
-
-      const now = new Date();
-
-      console.log(`Current time: '${now}'. Expression: '${expression}'. Timezone: '${timezone}'.`);
-
-      if(!cr.isDateAllowed(now)) {
-        utils.build.cancelBuild('Deployment not allowed at this time.');
-      }
+    let cr;
+    try {
+      cr = createCronAllowedRange(expression, timezone);
+    } catch (error) {
+      utils.build.failPlugin('Invalid Cron expression or timezone', { error });
+      return;
     }
+
+    const now = new Date();
+
+    console.log(`Current time: '${now}'. Expression: '${expression}'. Timezone: '${timezone}'.`);
+
+    if(!cr.isDateAllowed(now)) {
+      utils.build.cancelBuild('Deployment not allowed at this time.');
+    }
+  }
 };
